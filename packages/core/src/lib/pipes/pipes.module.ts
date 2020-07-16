d5850f1c32f6696cfba765931d2741c16241d90b
Julia Samól <j.samol@papers.ch>
2020-07-16 17:20:26 +0200

    fix(core): fix import paths

--- packages/core/src/lib/pipes/pipes.module.ts
+++ packages/core/src/lib/pipes/pipes.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core'
 import { AmountConverterPipe } from './amount-converter/amount-converter.pipe';
-import { FeeConverterPipe } from './fee-converter.pipe'
+import { FeeConverterPipe } from './fee-converter/fee-converter.pipe'
 
 @NgModule({
   declarations: [AmountConverterPipe, FeeConverterPipe],
