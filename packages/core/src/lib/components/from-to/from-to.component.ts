1f1dc480668ca2fa3096f878069e1dd04188d711
Julia Samól <j.samol@papers.ch>
2020-07-17 10:53:13 +0200

    feat(core): transform transaction details

--- packages/core/src/lib/components/from-to/from-to.component.ts
+++ packages/core/src/lib/components/from-to/from-to.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core'
+import * as moment from 'moment'
 import { IAirGapTransaction } from 'airgap-coin-lib'
+import { AmountConverterPipe } from '../../pipes/amount-converter/amount-converter.pipe'
 
 @Component({
   selector: 'airgap-from-to',
@@ -12,6 +14,25 @@ export class FromToComponent {
   
   @Input()
   public displayRawData: boolean = false
+
+  public get transactionAmount(): string {
+    return this.amountConverterPipe.transform(this.transaction?.amount, { protocol: this.transaction?.protocolIdentifier })
+  }
+
+  public get transactionFee(): string {
+    return this.amountConverterPipe.transform(this.transaction?.fee, { protocol: this.transaction?.protocolIdentifier })
+  }
+
+  public get transactionTimestamp(): string {
+    return this.transaction !== undefined && this.transaction.timestamp !== undefined
+      ? moment.unix(this.transaction.timestamp).calendar(null, {
+          sameDay: '[Today at] HH:mm',
+          sameElse: 'HH:mm [on] LL'
+        })
+      : ''
+  }
+
+  constructor(private readonly amountConverterPipe: AmountConverterPipe) {}
 
   public toggleDisplayRawData(): void {
     this.displayRawData = !this.displayRawData
