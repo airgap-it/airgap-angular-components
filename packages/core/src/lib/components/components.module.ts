2d875cab0159b0101f05bf746ac6004d991cb3bd
Julia Samól <j.samol@papers.ch>
2020-07-22 12:30:06 +0200

    feat(from-to): use pipes directly in the template

--- packages/core/src/lib/components/components.module.ts
+++ packages/core/src/lib/components/components.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
 import { IonicModule } from '@ionic/angular'
 import { TranslateModule } from '@ngx-translate/core'
+import { MomentModule } from 'ngx-moment'
 
 import { PipesModule } from '../pipes/pipes.module'
 import { CurrencySymbolComponent } from './currency-symbol/currency-symbol.component'
@@ -13,7 +14,7 @@ import { TitledTextComponent } from './titled-text/titled-text.component'
 
 @NgModule({
   declarations: [FromToComponent, IdenticonComponent, TitledTextComponent, TitledAddressComponent, CurrencySymbolComponent, NetworkBadgeComponent],
-  imports: [CommonModule, IonicModule, TranslateModule, PipesModule],
+  imports: [CommonModule, IonicModule, TranslateModule, MomentModule, PipesModule],
   exports: [FromToComponent, IdenticonComponent, TitledTextComponent, TitledAddressComponent, CurrencySymbolComponent, NetworkBadgeComponent]
 })
 export class ComponentsModule {}
--- packages/core/src/lib/components/from-to/from-to.component.ts
+++ packages/core/src/lib/components/from-to/from-to.component.ts
@@ -1,7 +1,5 @@
 import { Component, Input } from '@angular/core'
-import * as moment from 'moment'
 import { IAirGapTransaction } from 'airgap-coin-lib'
-import { AmountConverterPipe } from '../../pipes/amount-converter/amount-converter.pipe'
 
 @Component({
   selector: 'airgap-from-to',
@@ -14,25 +12,6 @@ export class FromToComponent {
   
   @Input()
   public displayRawData: boolean = false
-
-  public get transactionAmount(): string {
-    return this.amountConverterPipe.transform(this.transaction?.amount, { protocol: this.transaction?.protocolIdentifier })
-  }
-
-  public get transactionFee(): string | undefined {
-    return this.transaction?.fee !== undefined ? this.amountConverterPipe.transform(this.transaction.fee, { protocol: this.transaction?.protocolIdentifier }) : undefined
-  }
-
-  public get transactionTimestamp(): string | undefined {
-    return this.transaction?.timestamp !== undefined
-      ? moment.unix(this.transaction.timestamp).calendar(null, {
-          sameDay: '[Today at] HH:mm',
-          sameElse: 'HH:mm [on] LL'
-        })
-      : undefined
-  }
-
-  constructor(private readonly amountConverterPipe: AmountConverterPipe) {}
 
   public toggleDisplayRawData(): void {
     this.displayRawData = !this.displayRawData
