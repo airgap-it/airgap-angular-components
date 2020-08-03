f7ed0dddb4bdb16b9a437db045991ca90e0bfe0a
Julia Samól <j.samol@papers.ch>
2020-08-03 16:54:29 +0200

    fix(core): remove default http loader for translations

--- packages/core/src/lib/airgap-common-core.module.ts
+++ packages/core/src/lib/airgap-common-core.module.ts
@@ -1,24 +1,13 @@
 import { NgModule } from '@angular/core'
-import { HttpClient } from '@angular/common/http'
-import { TranslateModule, TranslateLoader } from '@ngx-translate/core'
+import { TranslateModule } from '@ngx-translate/core'
 import { ComponentsModule } from './components/components.module'
 import { PipesModule } from './pipes/pipes.module'
 import { AmountConverterPipe } from './pipes/amount-converter/amount-converter.pipe'
-import { AirGapCommonTranslateLoader } from './translation/AirGapCommonTranslateLoader'
-
-export function createTranslateLoader(httpClient: HttpClient): AirGapCommonTranslateLoader {
-  return new AirGapCommonTranslateLoader(httpClient)
-}
 
 @NgModule({
   declarations: [],
   imports: [
     TranslateModule.forChild({
-      loader: {
-        provide: TranslateLoader,
-        useFactory: createTranslateLoader,
-        deps: [HttpClient]
-      },
       isolate: false
     }),
     ComponentsModule, 
