ecc5627d03038d241628c0e708dcaf39ffad2f29
AndreasGassmann <andreas@andreasgassmann.ch>
2020-08-10 14:18:54 +0200

    fix(test): fee-converter

--- packages/core/src/lib/pipes/fee-converter/fee-converter.pipe.ts
+++ packages/core/src/lib/pipes/fee-converter/fee-converter.pipe.ts
@@ -14,22 +14,28 @@ interface FeeConverterArgs {
   name: 'feeConverter'
 })
 export class FeeConverterPipe implements PipeTransform {
-
   constructor(private readonly protocolsService: ProtocolService) {}
 
   public transform(value: FeeConverterValue, args: FeeConverterArgs): string {
-    if (args.protocol === undefined || value === null || value === undefined) {
-      return ''
+    if (args.protocol === undefined || !args.protocol) {
+      throw new Error('Invalid protocol')
+    }
+
+    if (!(typeof value === 'string' || typeof value === 'number' || BigNumber.isBigNumber(value))) {
+      throw new Error('Invalid fee amount')
     }
 
     const protocol: ICoinProtocol | undefined = this.protocolsService.getProtocol(args.protocol)
     if (protocol === undefined) {
-      return ''
+      throw new Error('Protocol not supported')
     }
 
     const fee: BigNumber = new BigNumber(value).shiftedBy(-protocol.feeDecimals)
 
+    if (fee.isNaN()) {
+      throw new Error('Invalid fee amount')
+    }
+
     return `${fee.toFixed()} ${protocol?.feeSymbol.toUpperCase()}`
   }
-
 }
