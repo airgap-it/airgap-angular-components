6858d032ecf67e68252cd1e40ea807d366f99139
AndreasGassmann <andreas@andreasgassmann.ch>
2020-08-10 14:27:01 +0200

    fix(test): language service

--- packages/core/src/lib/services/language/language.service.spec.ts
+++ packages/core/src/lib/services/language/language.service.spec.ts
@@ -1,14 +1,18 @@
-import { TestBed } from '@angular/core/testing'
+import { TestBed, async } from '@angular/core/testing'
 
 import { LanguageService } from './language.service'
+import { TestBedUtils } from '../../../../test/utils/test-bed'
 
 describe('LanguageService', () => {
   let service: LanguageService
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({})
+  let testBedUtils: TestBedUtils
+
+  beforeEach(async(() => {
+    testBedUtils = new TestBedUtils()
+    TestBed.configureTestingModule(testBedUtils.moduleDef({}))
     service = TestBed.inject(LanguageService)
-  })
+  }))
 
   it('should be created', () => {
     expect(service).toBeTruthy()
