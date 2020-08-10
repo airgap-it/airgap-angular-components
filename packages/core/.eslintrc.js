f10024a509b2eeb6e1712ccac32543f421f2bd86
Julia Samól <j.samol@papers.ch>
2020-08-10 13:37:18 +0200

    chore(eslint): setup eslint scripts

--- packages/core/.eslintrc.js
+++ packages/core/.eslintrc.js
@@ -2,10 +2,11 @@ module.exports = {
   extends: "../../.eslintrc.js",
   parserOptions: {
     project: [
-      './packages/core/tsconfig.lib.json',
-      './packages/core/tsconfig.lib.prod.json',
-      './packages/core/tsconfig.spec.json'
+      './tsconfig.lib.json',
+      './tsconfig.lib.prod.json',
+      './tsconfig.spec.json'
     ],
-    sourceType: 'module'
+    sourceType: 'module',
+    tsconfigRootDir: __dirname
   }
 }
