99d10fc864489ac759fbe3f85b9cca553d78ec65
Julia Samól <j.samol@papers.ch>
2020-08-12 10:12:06 +0200

    chore(): change versioning from independent to common

--- npm-ci-publish.sh
+++ npm-ci-publish.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "//registry.npmjs.org/:_authToken=$NPM_AUTH_TOKEN" > .npmrc
 
-VERSION=$(node -pe 'JSON.parse(process.argv[1]).version.indexOf("beta")' "$(cat package.json)")
+VERSION=$(node -pe 'JSON.parse(process.argv[1]).version.indexOf("beta")' "$(cat lerna.json)")
 
 if [ "$VERSION" = "-1" ]
 then
