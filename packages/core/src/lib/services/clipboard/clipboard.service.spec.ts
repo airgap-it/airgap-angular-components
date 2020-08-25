5d5408f4d438a0a319ef3ab2d0dfb462a74638b8
AndreasGassmann <andreas@andreasgassmann.ch>
2020-08-25 10:05:33 +0200

    fix(clipboard): add mocks

--- packages/core/src/lib/services/clipboard/clipboard.service.spec.ts
+++ packages/core/src/lib/services/clipboard/clipboard.service.spec.ts
@@ -1,15 +1,23 @@
 import { TestBed, async } from '@angular/core/testing'
 
 import { TestBedUtils } from '../../../../test/utils/test-bed'
+import { ClipboardMock } from '../../../../test/utils/plugins-mock'
+import { CLIPBOARD_PLUGIN } from '../../capacitor-plugins/injection-tokens'
 import { ClipboardService } from './clipboard.service'
 
 describe('ClipboardService', () => {
   let service: ClipboardService
   let testBedUtils: TestBedUtils
 
+  const clipboardPluginMock = new ClipboardMock()
+
   beforeEach(async(() => {
     testBedUtils = new TestBedUtils()
-    TestBed.configureTestingModule(testBedUtils.moduleDef({}))
+    TestBed.configureTestingModule(
+      testBedUtils.moduleDef({
+        providers: [{ provide: CLIPBOARD_PLUGIN, useValue: clipboardPluginMock }]
+      })
+    )
     service = TestBed.inject(ClipboardService)
   }))
 
---
+++ packages/core/test/utils/plugins-mock.ts
@@ -0,0 +1,24 @@
+/* eslint-disable max-classes-per-file */
+import { newSpy } from './unit-test-helper'
+
+export class AppMock {
+  public addListener = newSpy('addListener', {})
+  public openUrl = newSpy('openUrl', Promise.resolve())
+}
+
+export class AppInfoMock {
+  public get: jasmine.Spy = newSpy(
+    'set',
+    Promise.resolve({
+      appName: 'AirGap.UnitTest',
+      packageName: 'AirGap',
+      versionName: '0.0.0',
+      versionCode: 0
+    })
+  )
+}
+
+export class ClipboardMock {
+  public read: jasmine.Spy = newSpy('read', Promise.resolve())
+  public write: jasmine.Spy = newSpy('write', Promise.resolve())
+}
