3261ffd3536741642367a92f81c11962976c78af
Mike Godenzi <m.godenzi@papers.ch>
2020-10-02 15:51:47 +0200

    chore(): removed snx from the erc20 supported tokens

--- packages/core/src/lib/services/protocol/tokens.ts
+++ packages/core/src/lib/services/protocol/tokens.ts
@@ -130,14 +130,14 @@ export const ethTokens: Token[] = [
     contractAddress: '0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2',
     decimals: 18
   },
-  {
-    symbol: 'SNX',
-    name: 'Synthetix Network Token',
-    marketSymbol: 'snx',
-    identifier: 'eth-erc20-snx',
-    contractAddress: '0xC011A72400E58ecD99Ee497CF89E3775d4bd732F',
-    decimals: 18
-  },
+  // {
+  //   symbol: 'SNX',
+  //   name: 'Synthetix Network Token',
+  //   marketSymbol: 'snx',
+  //   identifier: 'eth-erc20-snx',
+  //   contractAddress: '0xC011A72400E58ecD99Ee497CF89E3775d4bd732F',
+  //   decimals: 18
+  // },
   {
     symbol: 'THETA',
     name: 'Theta Token',
