3e5d0cc022057ed1ca4c06d459c27e785feceaee
Julia Samol <j.samol@papers.ch>
2021-10-25 13:05:35 +0200

    chore/update-android-12

--- packages/core/src/lib/capacitor-plugins/injection-tokens.ts
+++ packages/core/src/lib/capacitor-plugins/injection-tokens.ts
@@ -1,11 +1,15 @@
 import { InjectionToken } from '@angular/core'
-import { AppPlugin, ClipboardPlugin, SplashScreenPlugin, StatusBarPlugin, PermissionsPlugin } from '@capacitor/core'
+import { AppPlugin } from '@capacitor/app'
+import { AppLauncherPlugin } from '@capacitor/app-launcher'
+import { ClipboardPlugin } from '@capacitor/clipboard'
+import { SplashScreenPlugin } from '@capacitor/splash-screen'
+import { StatusBarPlugin } from '@capacitor/status-bar'
 
 import { AppInfoPlugin } from './definitions'
 
 export const APP_PLUGIN = new InjectionToken<AppPlugin>('AppPlugin')
 export const APP_INFO_PLUGIN = new InjectionToken<AppInfoPlugin>('AppInfoPlugin')
+export const APP_LAUNCHER_PLUGIN = new InjectionToken<AppLauncherPlugin>('AppLauncherPlugin')
 export const CLIPBOARD_PLUGIN = new InjectionToken<ClipboardPlugin>('ClipboardPlugin')
-export const PERMISSIONS_PLUGIN = new InjectionToken<PermissionsPlugin>('PermissionsPlugin')
 export const SPLASH_SCREEN_PLUGIN = new InjectionToken<SplashScreenPlugin>('SplashScreenPlugin')
 export const STATUS_BAR_PLUGIN = new InjectionToken<StatusBarPlugin>('StatusBarPlugin')
